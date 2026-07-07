4ce8d99a0477c6ede41155a8e410334f188f71a5
Danilo Sena <aluno.den@senaibahia.com.br>
2026-07-07 20:58:09 -0300

    Atualização app jsx

--- front-end-AOE/src/App.jsx
+++ front-end-AOE/src/App.jsx
@@ -1,8 +1,14 @@
 import './App.css'
 import Home from './pages/Home'
+import Contato from './pages/Contato'
 
 function App() {
-  return <Home />
+  return (
+    <>
+      <Home/>
+      <Contato/>
+    </>
+  )
 }
 
 export default App
