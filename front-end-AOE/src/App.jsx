b28c4bdf14a69fb73e7122f6cb9195ea122211ef
Danilo Sena <aluno.den@senaibahia.com.br>
2026-07-08 19:40:25 -0300

    atualizando por conta de erro

--- front-end-AOE/src/App.jsx
+++ front-end-AOE/src/App.jsx
@@ -5,7 +5,7 @@ import Footer from './components/Footer'
 import Home from './pages/Home'
 import Contato from './pages/Contato'
 import QuemSomos from './pages/QuemSomos'
-import HSE from './pages/HSE'
+import HSE from './pages/Incidentes'
 import Producao from './pages/Producao'
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/quem-somos" element={<QuemSomos />} />
-        <Route path="/hse" element={<HSE />} />
+        <Route path="/incidentes" element={<incidentes />} />
         <Route path="/producao" element={<Producao />} />
         <Route path="/contato" element={<Contato />} />
       </Routes>
