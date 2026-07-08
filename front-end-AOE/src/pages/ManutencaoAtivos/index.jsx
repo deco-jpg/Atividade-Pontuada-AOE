31e92f811637c8e191c6f0b920cb5a8ed00ea3f5
Danilo Sena <aluno.den@senaibahia.com.br>
2026-07-08 19:43:24 -0300

    atualizando por conta de erro

--- front-end-AOE/src/pages/ManutencaoAtivos/index.jsx
+++ front-end-AOE/src/pages/ManutencaoAtivos/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./style.css";
+import "./styles.css";
 
 export default function Manutencoes() {
   // Estado centralizado espelhando a entidade Java Spring Boot
