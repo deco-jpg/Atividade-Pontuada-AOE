7891510d1fc0b765a8596b350b29d963f60a052c
Danilo Sena <aluno.den@senaibahia.com.br>
2026-07-07 21:19:26 -0300

    Att quemsomos

--- front-end-AOE/src/App.jsx
+++ front-end-AOE/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Home from './pages/Home'
 import Contato from './pages/Contato'
+import QuemSomos from './pages/QuemSomos'
 
 function App() {
   return (
     <>
       <Home/>
       <Contato/>
+      <QuemSomos/>
     </>
   )
 }
